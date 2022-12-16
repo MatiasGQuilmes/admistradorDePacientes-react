936c5441807b5e11f420701a3fcb31f8619edc4c
matias <matyyassgonzalez89@gmail.com>
2022-12-16 13:03:16 -0300

    libreria short id implementada para generar y asignar ids

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,16 +1,21 @@
 import React, { Fragment , useState } from 'react';
+import shortid from 'shortid';
+
 
 
 const Formulario = () => {
 
     // Crear State de Citas
     const [cita, actualizarCita] = useState({
+        id: '',
         mascota: '',
         propietario: '',
         fecha: '',
         hora: '',
         sintomas: ''
     });
+
+    const [error, actualizarError] = useState(false);
 
     //Funcion que se ejecuta cuando el usuario escribe en un input
     const actualizarState = e => {
@@ -20,11 +25,43 @@ const Formulario = () => {
         })
     }
 
+    // Extraer los valores
+    const { mascota, propietario, fecha, hora, sintomas } = cita;
+
+    // Agregar turno
+    const submitTurno = e => {
+        e.preventDefault();
+
+        // Validar datos
+        if (mascota.trim() === '' || propietario.trim() === '' || fecha.trim() === '' || hora.trim() === '' || sintomas.trim() === 
+        ''  ) {
+            actualizarError(true);
+            return; //si hay un problema no sigue ejecutando el codigo
+        }
+
+        // Eliminar el mensaje previo
+        actualizarError(false);
+
+        // generando y asignando id
+        cita.id = shortid.generate(); //asigna automaticamente un id con la libreria shortid, no use uuid
+        console.log(cita);
+       
+        // Creando la cita
+
+        
+        // Reiniciar el form
+    }
+
+
     return ( 
         <Fragment>
             <h2>Crear Cita</h2>
+            
+            {error ?   <p className='alerta-error'>Todos los campos son obligatorios!</p>  : null}
 
-            <form>
+            <form
+                onSubmit={submitTurno}
+            >
                 <label>Nombre Mascota</label>
                 <input
                     type="text"
@@ -32,6 +69,7 @@ const Formulario = () => {
                     className='u-full-width'
                     placeholder='Nombre Mascota' 
                     onChange={actualizarState}
+                    value={mascota}
                  />
 
                 <label>Nombre Dueño</label>
@@ -41,6 +79,7 @@ const Formulario = () => {
                     className='u-full-width'
                     placeholder='Nombre Dueño de la mascota' 
                     onChange={actualizarState}
+                    value={propietario}
                  />
 
                 <label>Fecha</label>
@@ -48,7 +87,8 @@ const Formulario = () => {
                     type="date"
                     name="fecha"
                     className='u-full-width'  
-                    onChange={actualizarState}              
+                    onChange={actualizarState} 
+                    value={fecha}             
                  />
 
                 <label>Hora</label>
@@ -57,6 +97,7 @@ const Formulario = () => {
                     name="hora"
                     className='u-full-width'
                     onChange={actualizarState}
+                    value={hora}
                  />
 
                 <label>Sintomas</label>
@@ -64,6 +105,7 @@ const Formulario = () => {
                     className='u-full-width'
                     name="sintomas"
                     onChange={actualizarState}
+                    value={sintomas}
                 ></textarea>
 
                 <button 
